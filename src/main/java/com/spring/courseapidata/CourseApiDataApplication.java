87d613948176f368ddd1f45f194ddfc471726dad
prabhsimran95 <32147436+prabhsimran95@users.noreply.github.com>
2018-10-13 03:20:21 -0400

    First commit

---
+++ src/main/java/com/spring/courseapidata/CourseApiDataApplication.java
@@ -0,0 +1,16 @@
+ package com.spring.courseapidata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiDataApplication {
+
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiDataApplication.class, args);
+	}
+}
